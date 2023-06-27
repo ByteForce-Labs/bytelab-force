5107ec2ba5287d5dca206240ce850072ca9c1639
Johnnnex <onyeiborjohn05@gmail.com>
2023-06-27 16:17:27 +0100

    layout

--- byteforce-labs/pages/layout/Layout.jsx
+++ byteforce-labs/pages/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Footer = () => {
+const Layout = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Footer = () => {
   )
 }
 
-export default Footer
+export default Layout
