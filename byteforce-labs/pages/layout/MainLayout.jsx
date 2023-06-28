b17ec6dd8234733e8fc3360808a5f2531c967869
Johnnnex <onyeiborjohn05@gmail.com>
2023-06-28 15:24:01 +0100

    nav

--- byteforce-labs/pages/layout/MainLayout.jsx
+++ byteforce-labs/pages/layout/MainLayout.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const MainLayout = () => {
+    const header = useRef()
+    useEffect(() =>{
+        window.addEventListener("scroll", () => {
+            header.current.classList.toggle("header-sticky", window.scrollY > 0)
+        })
+    }, [])
   return (
-    <header className=" fixed top-0 left-0 w-[100%] bg-white flex justify-between p-[1rem]">
+    <header ref={header} className=" fixed top-0 left-0 w-[100%] flex justify-between py-[2rem] px-[1rem]">
         <h1 className="font-poppins font-[900] text-[1.1rem]">Byteforce Labs</h1>
         <ul className=" font-spaceGrotesk font-bold flex gap-3">
             <li>WORK</li>
